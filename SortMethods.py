6e1d427aeed1cec6ae2adf7e5d60f9610d56ff0c
Christian Sweet <christian.t.sweet@hotmail.com>
2017-04-15 14:37:24 -0500

    Update SortMethods.py
    Small tweak with order of functions.

--- SortMethods.py
+++ SortMethods.py
@@ -5,15 +5,44 @@ import copy
 x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 y = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
+def sortPeakSupport(attrValues):
+        """
+        Function: Sorts into a Peak shape.
+        Returns: (List of) New order of values passed in.
+        """
+        
+        count = 0
+        z = copy.deepcopy(attrValues)
+        z.sort()
+        attrValuesHalf1 = []
+        attrValuesHalf2 = []
+        
+        for i in range(len(z)):
+                if count % 2:
+                        attrValuesHalf1.append(z[-1])
+                else:
+                        attrValuesHalf2.append(z[-1])
+                
+                z.remove(z[-1])
+                count+=1
+        
+        attrValuesHalf2 = attrValuesHalf2[::-1]
+        z = attrValuesHalf2 + attrValuesHalf1
+        
+        return z
 
-def sortPeakFun2(songIDs, attrValues):
+
+#-----------------------------------------------------------------------#
+
+
+def sortPeakFunc(songIDs, attrValues):
         """
         Function: Sorts into a Peak shape.
         Returns: (List of) New order of song keys.
         """
         
         d = dict(zip(attrValues, songIDs))
-        attrValuesMod = sortPeakFunc(attrValues)
+        attrValuesMod = sortPeakSupport(attrValues)
         songIDsMod = []
         
         for i in range(len(attrValuesMod)):
@@ -59,32 +88,4 @@ def sortToLeastFunc(songIDs, attrValues):
 
 sortToGreatestFunc(x, y)
 sortToLeastFunc(x, y)
-sortPeakFun2(x, y)
-#-----------------------------------------------------------------------#
-
-
-def sortPeakFunc(attrValues):
-        """
-        Function: Sorts into a Peak shape.
-        Returns: (List of) New order of values passed in.
-        """
-        
-        count = 0
-        z = copy.deepcopy(attrValues)
-        z.sort()
-        attrValuesHalf1 = []
-        attrValuesHalf2 = []
-        
-        for i in range(len(z)):
-                if count % 2:
-                        attrValuesHalf1.append(z[-1])
-                else:
-                        attrValuesHalf2.append(z[-1])
-                
-                z.remove(z[-1])
-                count+=1
-        
-        attrValuesHalf2 = attrValuesHalf2[::-1]
-        z = attrValuesHalf2 + attrValuesHalf1
-        
-        return z
+sortPeakFunc(x, y)
